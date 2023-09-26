2b4081868aec93e2f228e3ea23ee2cdb04a02e15
vanhuutoan27 <vhtoan27@gmail.com>
2023-09-26 16:35:12 +0700

    React Hook - useEffect - useContext

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,71 @@
 import React from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
 export default function Navigation() {
+  const { theme, toggle, dark } = useContext(ThemeContext);
   return (
-    <nav>
-      <ul>
-        <li>
-          <a className="active" href="#home">
-            Home
+    <div>
+      <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+        <ul>
+          <li>
+            <a
+              className="active"
+              href="#home"
+              style={{
+                color: theme.color,
+              }}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#news"
+              style={{
+                color: theme.color,
+              }}
+            >
+              News
+            </a>
+          </li>
+          <li>
+            <a
+              href="#about"
+              style={{
+                color: theme.color,
+              }}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              style={{
+                color: theme.color,
+              }}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div style={{ position: 'relative' }}>
+          <a
+            className="switch-mode"
+            href="#"
+            onClick={toggle}
+            style={{
+              backgroundColor: theme.backgroundColor,
+              color: theme.color,
+              outline: 'none',
+            }}
+            data-testid="toggle-theme-btn"
+          >
+            Switch Nav to {!dark ? 'Dark' : 'Light'} mode
           </a>
-        </li>
-        <li>
-          <a href="#news">News</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+        </div>
+      </nav>
+    </div>
   );
 }
