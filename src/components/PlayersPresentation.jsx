9ca0591074d45a64523a91dcdd4a13e45722b73d
vanhuutoan27 <vhtoan27@gmail.com>
2023-09-26 16:06:41 +0700

    React Hook - useState

--- src/components/PlayersPresentation.jsx
+++ src/components/PlayersPresentation.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
+import { Players } from '../shared/ListOfPlayers';
+import { useState } from 'react';
+
 export default function PlayersPresentation({ players }) {
-  //console.log(players);
+  const [player, setPlayer] = useState([]);
+
   return (
     <div className="container">
       {players.map((player) => (
-        <div className="column">
+        <div className="column" key={player.id}>
           <div className="card">
-            <img src={player.img} />
+            <img src={player.img} alt="" />
             <h3>{player.name}</h3>
-            <p className="title">{player.club}</p>
+            <p className="title"></p>
             <p>
-              <button>Detail</button>
+              <a href="#popup1" id="openPopUp">
+                <button
+                  onClick={() => {
+                    setPlayer(player);
+                  }}
+                >
+                  Detail
+                </button>
+              </a>
             </p>
           </div>
         </div>
       ))}
+
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} alt="" />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
   );
 }
