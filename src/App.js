92f1658fb907de0b86b1ad51c3a61b9873a76d63
vanhuutoan27 <vhtoan27@gmail.com>
2023-09-26 14:39:13 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import Player from './components/Players';
-
+import Main from './components/Main';
+import Navigation from './components/Navigation';
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Player />
+      <Main />
       <Footer />
     </div>
   );
 }
-
 export default App;
