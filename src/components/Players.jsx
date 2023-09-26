c7b70564f18b06b475f3c56c247c5a90d35300a6
vanhuutoan27 <vhtoan27@gmail.com>
2023-09-26 14:32:13 +0700

    Functional Components

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Players } from '../shared/ListOfPlayers';
 
-export default class Player extends Component {
-  render() {
-    return (
-      <div className="container">
-        {Players.map((player) => (
-          <div className="column" key={player.id}>
-            <div className="card">
-              <img src={player.img} />
-              <h3>{player.name}</h3>
-              <p className="title">{player.club}</p>
-              <p>
-                <button>Detail</button>
-              </p>
-            </div>
+export default function Player() {
+  return (
+    <div className="container">
+      {Players.map((player) => (
+        <div className="column" key={player.id}>
+          <div className="card">
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className="title">{player.club}</p>
+            <p>
+              <button>Detail</button>
+            </p>
           </div>
-        ))}
-      </div>
-    );
-  }
+        </div>
+      ))}
+    </div>
+  );
 }
